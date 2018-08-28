e8f9b01b2df343fe12a5ca54229261aa8392de60
Ycarus <ycarus@zugaina.org>
2018-08-28 18:55:12 +0200

    Update sign.sh

--- sign.sh
+++ sign.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 key=${1:-key-build}
-
-[ -d source/bin ] && [ -f "$key" ] && \
-	find source/bin \
+path=${2:-x86_64}
+[ -d $path/source/bin ] && [ -f "$key" ] && \
+	find $path/source/bin \
 	\( -name '*.img.gz' -or -name 'Packages' \) \
-	-exec source/staging_dir/host/bin/usign -S -m {} -s "$key" \;
+	-exec $path/source/staging_dir/host/bin/usign -S -m {} -s "$key" \;
