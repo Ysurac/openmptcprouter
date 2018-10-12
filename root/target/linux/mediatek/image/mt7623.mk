edca3f42db3d816050c6c3c406fa00cb2ed5085b
Ycarus <ycarus@zugaina.org>
2018-10-12 20:53:39 +0200

    Fix rename BPI R2 image from .bin to .img

--- root/target/linux/mediatek/image/mt7623.mk
+++ root/target/linux/mediatek/image/mt7623.mk
@@ -16,8 +16,8 @@ define Device/7623n-bananapi-bpi-r2
   SUPPORTED_DEVICES := bananapi,bpi-r2
   IMAGES := sysupgrade.tar sysupgrade-sd.img.gz sysupgrade-emmc.img.gz
   IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
-  IMAGE/sysupgrade-sd.bin.gz := sysupgrade-bpi-r2-sd | gzip | append-metadata
-  IMAGE/sysupgrade-emmc.bin.gz := sysupgrade-bpi-r2-emmc | gzip | append-metadata
+  IMAGE/sysupgrade-sd.img.gz := sysupgrade-bpi-r2-sd | gzip | append-metadata
+  IMAGE/sysupgrade-emmc.img.gz := sysupgrade-bpi-r2-emmc | gzip | append-metadata
 endef
 
 TARGET_DEVICES += 7623n-bananapi-bpi-r2
