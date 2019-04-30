8b44fd6d74a921cb423eaab0733e90eb16bf4f1d
Ycarus <ycarus@zugaina.org>
2019-04-30 19:04:42 +0200

    Disable kernel 4.19 for now

--- build.sh
+++ build.sh
@@ -171,15 +171,15 @@ echo "Done"
 #echo "Remove old RPI firmware"
 #rm -rf target/linux/brcm2708/base-files/lib/firmware
 #echo "Done"
-echo "Set to kernel 4.19 for rpi arch"
-find target/linux/brcm2708 -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
-echo "Done"
-echo "Set to kernel 4.19 for x86 arch"
-find target/linux/x86 -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
-echo "Done"
-echo "Set to kernel 4.19 for mvebu arch (WRT)"
-find target/linux/mvebu -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
-echo "Done"
+#echo "Set to kernel 4.19 for rpi arch"
+#find target/linux/brcm2708 -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
+#echo "Done"
+#echo "Set to kernel 4.19 for x86 arch"
+#find target/linux/x86 -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
+#echo "Done"
+#echo "Set to kernel 4.19 for mvebu arch (WRT)"
+#find target/linux/mvebu -type f -name Makefile -exec sed -i 's%KERNEL_PATCHVER:=4.14%KERNEL_PATCHVER:=4.19%g' {} \;
+#echo "Done"
 
 
 
