e936a82cd5a1945a54035e86146bfe5e5d771906
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2020-04-03 21:18:24 +0200

    Update bpi-r2 config

--- root/target/linux/mediatek/image/mt7623.mk
+++ root/target/linux/mediatek/image/mt7623.mk
@@ -1,16 +1,19 @@
-define Device/7623a-unielec-u7623-02-emmc-512m
-  DEVICE_TITLE := MTK7623a UniElec U7623-02 (eMMC/512MB RAM)
-  DEVICE_DTS := mt7623a-unielec-u7623-02-emmc-512M
+define Device/unielec-u7623-02-emmc-512m
+  DEVICE_VENDOR := UniElec
+  DEVICE_MODEL := U7623-02
+  DEVICE_VARIANT := eMMC/512MB RAM
+  DEVICE_DTS := mt7623a-unielec-u7623-02-emmc-512m
   DEVICE_PACKAGES := mkf2fs e2fsprogs kmod-fs-vfat kmod-nls-cp437 kmod-nls-iso8859-1 kmod-mmc
   SUPPORTED_DEVICES := unielec,u7623-02-emmc-512m
   IMAGES := sysupgrade-emmc.bin.gz
   IMAGE/sysupgrade-emmc.bin.gz := sysupgrade-emmc | gzip | append-metadata
 endef
 
-TARGET_DEVICES += 7623a-unielec-u7623-02-emmc-512m
+TARGET_DEVICES += unielec-u7623-02-emmc-512m
 
 define Device/7623n-bananapi-bpi-r2
-  DEVICE_TITLE := MTK7623n BananaPi R2
+  DEVICE_VENDOR := Bpi
+  DEVICE_MODEL := Banana Pi R2
   DEVICE_DTS := mt7623n-bananapi-bpi-r2
 #  DEVICE_PACKAGES := wmt uboot-mtk-bpi-r2 kmod-crypto-hw-mtk kmod-nat-hw-mtk
   DEVICE_PACKAGES := wmt uboot-mtk-bpi-r2 kmod-crypto-hw-mtk kmod-mt6625l-wlan-gen2 kmod-usb-core kmod-ata-core kmod-usb3 kmod-usb2 kmod-usb-ohci mt7623n-preloader
