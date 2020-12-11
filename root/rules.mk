15b5e54963397310b21a2c262db809ae98f64c6c
suyuan <175338101@qq.com>
2020-12-11 22:31:03 +0800

    update rules.mk
    fix rules.mk

--- root/rules.mk
+++ root/rules.mk
@@ -174,8 +174,6 @@ TARGET_CFLAGS:=$(TARGET_OPTIMIZATION)$(if $(CONFIG_DEBUG), -g3) $(call qstrip,$(
 TARGET_CXXFLAGS = $(TARGET_CFLAGS)
 TARGET_ASFLAGS_DEFAULT = $(TARGET_CFLAGS)
 TARGET_ASFLAGS = $(TARGET_ASFLAGS_DEFAULT)
-TARGET_CPPFLAGS:=-I$(STAGING_DIR)/usr/include
-TARGET_LDFLAGS:=-L$(STAGING_DIR)/usr/lib -L$(STAGING_DIR)/lib
 ifneq ($(CONFIG_EXTERNAL_TOOLCHAIN),)
 LIBGCC_S_PATH=$(realpath $(wildcard $(call qstrip,$(CONFIG_LIBGCC_ROOT_DIR))/$(call qstrip,$(CONFIG_LIBGCC_FILE_SPEC))))
 LIBGCC_S=$(if $(LIBGCC_S_PATH),-L$(dir $(LIBGCC_S_PATH)) -lgcc_s)
@@ -334,9 +332,9 @@ else
   endif
   RSTRIP= \
     export CROSS="$(TARGET_CROSS)" \
-    $(if $(PKG_BUILD_ID),KEEP_BUILD_ID=1) \
-    $(if $(CONFIG_KERNEL_KALLSYMS),NO_RENAME=1) \
-    $(if $(CONFIG_KERNEL_PROFILING),KEEP_SYMBOLS=1); \
+		$(if $(PKG_BUILD_ID),KEEP_BUILD_ID=1) \
+		$(if $(CONFIG_KERNEL_KALLSYMS),NO_RENAME=1) \
+		$(if $(CONFIG_KERNEL_PROFILING),KEEP_SYMBOLS=1); \
     NM="$(TARGET_CROSS)nm" \
     STRIP="$(STRIP)" \
     STRIP_KMOD="$(SCRIPT_DIR)/strip-kmod.sh" \
@@ -372,10 +370,10 @@ endef
 # $(2) => The lock name. If not given, the global lock will be used.
 ifneq ($(wildcard $(STAGING_DIR_HOST)/bin/flock),)
   define locked
-  SHELL= \
-  flock \
-    $(TMP_DIR)/.$(if $(2),$(strip $(2)),global).flock \
-    -c '$(subst ','\'',$(1))'
+	SHELL= \
+	flock \
+		$(TMP_DIR)/.$(if $(2),$(strip $(2)),global).flock \
+		-c '$(subst ','\'',$(1))'
   endef
 else
   locked=$(1)
@@ -386,25 +384,25 @@ endif
 # $(1) => File glob expression
 # $(2) => Destination directory
 define file_copy
-  for src_dir in $(sort $(foreach d,$(wildcard $(1)),$(dir $(d)))); do \
-    ( cd $$src_dir; find -type f -or -type d ) | \
-      ( cd $(2); while :; do \
-        read FILE; \
-        [ -z "$$FILE" ] && break; \
-        [ -L "$$FILE" ] || continue; \
-        echo "Removing symlink $(2)/$$FILE"; \
-        rm -f "$$FILE"; \
-      done; ); \
-  done; \
-  $(CP) $(1) $(2)
+	for src_dir in $(sort $(foreach d,$(wildcard $(1)),$(dir $(d)))); do \
+		( cd $$src_dir; find -type f -or -type d ) | \
+			( cd $(2); while :; do \
+				read FILE; \
+				[ -z "$$FILE" ] && break; \
+				[ -L "$$FILE" ] || continue; \
+				echo "Removing symlink $(2)/$$FILE"; \
+				rm -f "$$FILE"; \
+			done; ); \
+	done; \
+	$(CP) $(1) $(2)
 endef
 
 # Calculate sha256sum of any plain file within a given directory
 # $(1) => Input directory
 # $(2) => If set, recurse into subdirectories
 define sha256sums
-  (cd $(1); find . $(if $(2),,-maxdepth 1) -type f -not -name 'sha256sums' -printf "%P\n" | sort | \
-    xargs -r $(STAGING_DIR_HOST)/bin/mkhash -n sha256 | sed -ne 's!^\(.*\) \(.*\)$$!\1 *\2!p' > sha256sums)
+	(cd $(1); find . $(if $(2),,-maxdepth 1) -type f -not -name 'sha256sums' -printf "%P\n" | sort | \
+		xargs -r $(STAGING_DIR_HOST)/bin/mkhash -n sha256 | sed -ne 's!^\(.*\) \(.*\)$$!\1 *\2!p' > sha256sums)
 endef
 
 # file extension
@@ -415,18 +413,18 @@ FORCE: ;
 .PHONY: FORCE
 
 check: FORCE
-  @true
+	@true
 
 val.%:
-  @$(if $(filter undefined,$(origin $*)),\
-    echo "$* undefined" >&2, \
-    echo '$(subst ','"'"',$($*))' \
-  )
+	@$(if $(filter undefined,$(origin $*)),\
+		echo "$* undefined" >&2, \
+		echo '$(subst ','"'"',$($*))' \
+	)
 
 var.%:
-  @$(if $(filter undefined,$(origin $*)),\
-    echo "$* undefined" >&2, \
-    echo "$*='"'$(subst ','"'\"'\"'"',$($*))'"'" \
-  )
+	@$(if $(filter undefined,$(origin $*)),\
+		echo "$* undefined" >&2, \
+		echo "$*='"'$(subst ','"'\"'\"'"',$($*))'"'" \
+	)
 
 endif #__rules_inc
