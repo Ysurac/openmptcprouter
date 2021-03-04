98b91532800952ed06f8cdaf44d39645c1b0b575
Olli <oliver@welter.rocks>
2021-03-05 00:58:24 +0100

    Fixed missing config items

--- build.sh
+++ build.sh
@@ -209,6 +209,13 @@ if [ "$OMR_TARGET" = "bpi-r1" ]; then
 	sed -i "s/CONFIG_MC_SUBSHELL=y/# CONFIG_MC_SUBSHELL is not set/" "$OMR_TARGET/source/.config"
 	sed -i "s/CONFIG_MC_CHARSET=y/# CONFIG_MC_CHARSET is not set/" "$OMR_TARGET/source/.config"
 	sed -i "s/CONFIG_MC_VFS=y/# CONFIG_MC_VFS is not set/" "$OMR_TARGET/source/.config"
+	
+	# Switch to wpad-wolfssl
+	sed -i "s/CONFIG_PACKAGE_wpad-basic=y/# CONFIG_PACKAGE_wpad-basic is not set/" "$OMR_TARGET/source/.config"
+	sed -i "s/CONFIG_PACKAGE_wpad-basic-wolfssl=m/CONFIG_PACKAGE_wpad-basic-wolfssl=y/" "$OMR_TARGET/source/.config"
+
+	# Enable sound soc sunxi kernel module
+	sed -i "s/CONFIG_PACKAGE_kmod-sound-soc-sunxi=m/CONFIG_PACKAGE_kmod-sound-soc-sunxi=y/" "$OMR_TARGET/source/.config"
 fi
 
 cd "$OMR_TARGET/source"
