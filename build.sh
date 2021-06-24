2b9ee90c1ece59cd68b33655614eed1925a90605
Ycarus (Yannick Chabanois) <ycarus@zugaina.org>
2021-06-24 17:09:41 +0200

    Update OpenWrt

--- build.sh
+++ build.sh
@@ -89,9 +89,9 @@ fi
 
 #_get_repo source https://github.com/ysurac/openmptcprouter-source "master"
 if [ "$OMR_OPENWRT" = "default" ]; then
-	_get_repo "$OMR_TARGET/source" https://github.com/openwrt/openwrt "b36068d35d9edbd8ed6aaeed6f4c863bfe4cfbee"
-	_get_repo feeds/packages https://github.com/openwrt/packages "1e3c22d81dec069893fec04784d4ce48a6b883cd"
-	_get_repo feeds/luci https://github.com/openwrt/luci "c615dd1387883faa76c0455c45db03438ea05e59"
+	_get_repo "$OMR_TARGET/source" https://github.com/openwrt/openwrt "1818b038d7275273adbd525b5ee76bc60b7d628c"
+	_get_repo feeds/packages https://github.com/openwrt/packages "fd62db1dc791deeb0d8a74cfcf0db425c6a5bdf7"
+	_get_repo feeds/luci https://github.com/openwrt/luci "4314d8f3300825a9c72e249250a60da860da5a7f"
 elif [ "$OMR_OPENWRT" = "master" ]; then
 	_get_repo "$OMR_TARGET/source" https://github.com/openwrt/openwrt "master"
 	_get_repo feeds/packages https://github.com/openwrt/packages "master"
@@ -391,12 +391,12 @@ echo "Done"
 #fi
 #echo "Done"
 
-echo "Checking if opkg install arguement too long patch is set or not"
-if ! patch -Rf -N -p1 -s --dry-run < ../../patches/package-too-long.patch; then
-	echo "apply..."
-	patch -N -p1 -s < ../../patches/package-too-long.patch
-fi
-echo "Done"
+#echo "Checking if opkg install arguement too long patch is set or not"
+#if ! patch -Rf -N -p1 -s --dry-run < ../../patches/package-too-long.patch; then
+#	echo "apply..."
+#	patch -N -p1 -s < ../../patches/package-too-long.patch
+#fi
+#echo "Done"
 
 echo "Download via IPv4"
 if ! patch -Rf -N -p1 -s --dry-run < ../../patches/download-ipv4.patch; then
