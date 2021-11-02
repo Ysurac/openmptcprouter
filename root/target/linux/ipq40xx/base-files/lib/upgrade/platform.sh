71b87065ca9db734e90865cf0308e999a39abfea
175338101@qq.com <175338101@qq.com>
2021-11-02 14:46:29 +0800

    fix

--- root/target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ root/target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -59,7 +59,6 @@ platform_do_upgrade() {
 	case "$(board_name)" in
 	8dev,jalapeno |\
 	aruba,ap-303 |\
-    pangu,l1000 |\
 	aruba,ap-303h |\
 	aruba,ap-365 |\
 	avm,fritzbox-7530 |\
@@ -67,15 +66,8 @@ platform_do_upgrade() {
 	avm,fritzrepeater-3000 |\
 	buffalo,wtr-m2133hp |\
 	cilab,meshpoint-one |\
-	edgecore,ecw5211 |\
-	edgecore,oap100 |\
 	engenius,eap2200 |\
-	glinet,gl-ap1300 |\
-	luma,wrtq-329acn |\
-	zbt,z4019 |\
 	mobipromo,cm520-79f |\
-    p2w,r619ac-64m |\
- 	p2w,r619ac-128m |\
 	qxwlan,e2600ac-c2)
 		nand_do_upgrade "$1"
 		;;
@@ -103,12 +95,13 @@ platform_do_upgrade() {
 		CI_UBIPART="ubifs"
 		askey_do_upgrade "$1"
 		;;
-	compex,wpj419)
+	compex,wpj419|\
+	p2w,r619ac-128m|\
+	p2w,r619ac)
 		nand_do_upgrade "$1"
 		;;
 	linksys,ea6350v3 |\
-	linksys,ea8300 |\
-	linksys,mr8300)
+	linksys,ea8300)
 		platform_do_upgrade_linksys "$1"
 		;;
 	meraki,mr33)
@@ -116,11 +109,9 @@ platform_do_upgrade() {
 		nand_do_upgrade "$1"
 		;;
 	openmesh,a42 |\
-	openmesh,a62 |\
-	plasmacloud,pa1200 |\
-	plasmacloud,pa2200)
+	openmesh,a62)
 		PART_NAME="inactive"
-		platform_do_upgrade_dualboot_datachk "$1"
+		platform_do_upgrade_openmesh "$1"
 		;;
 	zyxel,nbg6617)
 		zyxel_do_upgrade "$1"
