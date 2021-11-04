0f536ae2f33aa5f8e86b4b031e3a4379c614fbcd
suyuan <175338101@qq.com>
2021-11-05 00:41:41 +0800

    fix 40xx

--- root/target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ root/target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -59,7 +59,7 @@ platform_do_upgrade() {
 	case "$(board_name)" in
 	8dev,jalapeno |\
 	aruba,ap-303 |\
-	pangu,l1000 |\
+    pangu,l1000 |\
 	aruba,ap-303h |\
 	aruba,ap-365 |\
 	avm,fritzbox-7530 |\
@@ -74,7 +74,7 @@ platform_do_upgrade() {
 	luma,wrtq-329acn |\
 	zbt,z4019 |\
 	mobipromo,cm520-79f |\
-	p2w,r619ac-64m |\
+    p2w,r619ac-64m |\
  	p2w,r619ac-128m |\
 	qxwlan,e2600ac-c2)
 		nand_do_upgrade "$1"
