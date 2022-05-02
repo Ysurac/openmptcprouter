14138522c3a7b2d0cbda93035facb19092468582
suyuan168 <175338101@qq.com>
2022-05-02 14:21:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ OMR_RELEASE=${OMR_RELEASE:-$(git describe --tags `git rev-list --tags --max-coun
 OMR_REPO=${OMR_REPO:-http://$OMR_HOST:$OMR_PORT/release/$OMR_RELEASE/$OMR_TARGET}
 
 OMR_FEED_URL="${OMR_FEED_URL:-https://github.com/suyuan168/openmptcprouter-feeds}"
-OMR_FEED_SRC="${OMR_FEED_SRC:-develop}"
+OMR_FEED_SRC="${OMR_FEED_SRC:-ipq60xx}"
 
 CUSTOM_FEED_URL="${CUSTOM_FEED_URL}"
 
